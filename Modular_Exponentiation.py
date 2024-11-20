38ce76c8defe748667a70688490d7f5da0cb2210
Nicole Nasengo <nasengonicole@gmail.com>
2024-11-20 12:55:29 +0300

    Public-Key Cryptography

---
+++ Modular_Exponentiation.py
@@ -0,0 +1,11 @@
+# Base, exponent, and modulus
+base = 101
+exponent = 17
+modulus = 22663
+
+# Using the built-in pow function for modular exponentiation
+result = pow(base, exponent, modulus)
+
+# Print the result
+print(result)
+
